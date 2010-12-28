2ebf60696611555a38dc4db26bd6e691ff0deb8b
Jan Horalik <jan.horalik@jetminds.com>
2010-12-28 16:11:39 +0100

    PROTOTYPE: starting multiple threads in C# and waiting for them to end

---
+++ ThreadingProt1/ThreadingProt1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadingProt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker w1 = new Worker(1);
+            Worker w2 = new Worker(2);
+            ThreadStart job1 = new ThreadStart(w1.Run);
+            ThreadStart job2 = new ThreadStart(w2.Run);
+            Thread t1 = new Thread(job1);
+            Thread t2 = new Thread(job2);
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
---
+++ ThreadingProt1/ThreadingProt1/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadingProt1
+{
+    public class Worker
+    {
+        int _index;
+        public Worker(int index) { _index = index; }
+        public void Run()
+        {
+            Thread.Sleep(5000);
+            System.Console.WriteLine("This is thread:"+_index.ToString());
+        }
+    }
+}
