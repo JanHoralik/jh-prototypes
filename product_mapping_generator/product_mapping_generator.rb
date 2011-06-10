b3b42e8baabe443ddc11a76266411ea053c22243
Jan Horalik <jan.horalik@jetminds.com>
2011-06-10 16:50:39 +0200

    Skeletons of test generator and test files added

---
+++ product_mapping_generator/product_mapping_generator.rb
@@ -0,0 +1,11 @@
+class ProductMappingGenerator
+
+  SOURCE_COLUMNS = [3,4,6]
+
+  def initialize
+
+	
+  end
+
+end
+
---
+++ product_mapping_generator/product_mapping_generator_test.rb
@@ -0,0 +1,29 @@
+require "test/unit"
+require "product_mapping_generator"
+
+ class ProductMappingGeneratorTest < Test::Unit::TestCase
+
+  def setup
+     @g = ProductMappingGenerator.new
+  end
+
+  def teardown
+  end
+
+  def test_load_product_info   
+         
+	  assert 1
+ 	    
+  end
+
+  def test_generate_prices
+  end
+
+  def test_generate_hqpg_mapping
+  end
+
+  def test_generate_rpg_mapping
+  end
+
+ end
+
