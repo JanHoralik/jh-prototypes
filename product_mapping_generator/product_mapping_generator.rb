f797dfbd982a6d499deedde3af723cc1b088c21f
Jan Horalik <jan.horalik@jetminds.com>
2011-06-10 18:09:23 +0200

    First unit test, data loading working with fake data

--- product_mapping_generator/product_mapping_generator.rb
+++ product_mapping_generator/product_mapping_generator.rb
@@ -2,10 +2,28 @@ class ProductMappingGenerator
 
   SOURCE_COLUMNS = [3,4,6]
 
+  attr_reader :productToPck, :countryToSalesOrg, :rpgs
+  
   def initialize
 
 	
   end
+
+  def set_fake_values
+
+	  @productToPck = {"10000088"  => "584",
+	 		   "100000939" => "4770"}
+
+          @countryToSalesOrg = { "GB" => ["7011","7021","7031"],
+		  		 "DK" => ["5411","5421"]}
+	  @rpgs = ["VTE","S32"]
+
+  end
+
+  def load
+
+	  set_fake_values
+  end 
 
 end
 
