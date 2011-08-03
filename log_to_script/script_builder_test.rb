27b099535927286b9ef829041db85855c60cf8df
Jan Horalik <jan.horalik@jetminds.com>
2011-08-03 12:13:12 +0200

    Support for multiple input log files

--- log_to_script/script_builder_test.rb
+++ log_to_script/script_builder_test.rb
@@ -14,7 +14,7 @@ class ScriptBuilderTest < Test::Unit::TestCase
 	def tearDown
 	end
 
-	def test_SimpleFile
+	def Dtest_SimpleFile
 
 		@b.addLog @inputFile1
 		scriptLines = @b.process
@@ -24,7 +24,7 @@ class ScriptBuilderTest < Test::Unit::TestCase
 		assert_equal "015 Logout ", scriptLines[-1]	
 	end
 
-	def test_FileWithTwoScripts
+	def Dtest_FileWithTwoScripts
 
 		@b.addLog @inputFile2
 		scriptLines = @b.process
@@ -35,7 +35,7 @@ class ScriptBuilderTest < Test::Unit::TestCase
 		assert_equal "019 Logout ", scriptLines[-1]	
 	end
 
-	def test_TwoFiles
+	def Dtest_TwoFiles
 
 		@b.addLog @inputFile1
 		@b.addLog @inputFile2
@@ -50,10 +50,14 @@ class ScriptBuilderTest < Test::Unit::TestCase
 
 		@b.addLog @inputFile3
 		scriptLines = @b.process
-		assert_equal 10, scripts.length
-	        assert_equal "", scripts[2]
-		assert_equal "", scripts[8]
-
+		assert_equal 10, scriptLines.length
+	        assert_equal "For all line items..", scriptLines[13]
+		assert_equal "015 Add new MLBF line item", scriptLines[14]
+		assert_equal "016 Filter LI", scriptLines[15]
+		assert_equal "017 Set field on LI 1", scriptLines[16]
+		assert_equal "..", scriptLines[17]
+		assert_equal "018 Price PA ", scriptLines[18]
+		assert_match "Logout", scriptLines[-1]
 	end
 
 	def DIStest_writeOutputOfMultipleFiles
